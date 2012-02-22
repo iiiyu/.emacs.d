9f28bb26c2d513be0bc456092636f911de410754
XiaoChenYu <yunsn0303@gmail.com>
2012-02-22 23:39:06 +0800

    add highlight-symbol

--- init.el
+++ init.el
@@ -12,3 +12,12 @@
 
 ;; fullscreen mode
 (global-set-key (kbd "M-RET") 'ns-toggle-fullscreen) 
+
+
+;;  设置高亮
+(add-to-list 'load-path "~/.emacs.d/highlight-symbol")
+(require 'highlight-symbol)
+(global-set-key [(control f3)] 'highlight-symbol-at-point)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [(meta f3)] 'highlight-symbol-prev)))
