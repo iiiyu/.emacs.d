5143ab0a54c38d792e910382330d99f9edc3ac91
ChenYu Xiao <yunsn0303@gmail.com>
2012-02-23 16:22:06 +0800

    增加ecb的鼠标支持

--- init.el
+++ init.el
@@ -46,3 +46,15 @@
 (provide 'init)
 
 
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
