04217a01ee813fe786c9a1f026f887d4a018d97d
ChenYu Xiao <yunsn0303@gmail.com>
2012-03-05 06:33:53 +0800

    修改tabbar快捷键为M-n M-p

--- init-tabbar.el
+++ init-tabbar.el
@@ -4,8 +4,8 @@
 (tabbar-mode 1)
 
 ;; tabbar 跳转快捷键绑定
-(global-set-key [(meta j)] 'tabbar-backward)
-(global-set-key [(meta k)] 'tabbar-forward)
+(global-set-key [(meta n)] 'tabbar-backward)
+(global-set-key [(meta p)] 'tabbar-forward)
 
 
 (provide 'init-tabbar)
