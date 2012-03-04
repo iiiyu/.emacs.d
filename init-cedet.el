826e6b6a79319579268e0aa99aaa37292ff56783
ChenYu Xiao <yunsn0303@gmail.com>
2012-03-05 05:47:39 +0800

    绑定函数跳转快捷键C-M-0 C-M-9

--- init-cedet.el
+++ init-cedet.el
@@ -11,5 +11,13 @@
 ;; ecb 设置
 (add-to-list 'load-path "~/.emacs.d/ecb")
 
+;; 跳转到函数然后跳回来
+;(require 'semantic/analyze/refs)
+(global-set-key (kbd "C-M-0") 'semantic-ia-fast-jump)
+(global-set-key (kbd "C-M-9") 'semantic-mrub-switch-tags)
+;(define-key semantic-mode-map [C-0] 'semantic-ia-fast-jump)
+;(define-key semantic-mode-map [C-f12] 'semantic-ia-fast-jump-or-back)
+;(define-key semantic-mode-map [S-f12] 'semantic-ia-fast-jump-back)
+
 
 (provide 'init-cedet)
